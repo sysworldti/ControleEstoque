484cbcacd3965cc370c8f64a29d3c344b9ee7dad
Ricardo Pereira <ricardo.iviz@outlook.com.br>
2020-11-16 22:19:18 -0300

    Projeto inicial do estudo de .NET Framework com C#

---
+++ ControleEstoque.MVC/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.MVC.Models
+{    
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Description = "Login do Usuário")]
+        [MaxLength(length: 100, ErrorMessage = "Não respeitou o tamanho permitido para o seu login")]
+        public string Email { get; set; }
+
+        [Display(Description = "Senha do Usuário")]        
+        public string Password { get; set; }
+
+        [Display(Description = "Indica se o usuário está ativo no sistema")]
+        public bool IsActive { get; set; }
+    }
+}
